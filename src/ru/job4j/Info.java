72694ce8752e689ef6c82d769269305a665c2b44
damirka303 <damirka303@gmail.com>
2020-03-24 23:51:34 +0300

    3. Арифметические операции.[#255019]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -3,5 +3,9 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("24.03.2020");
+        int age = 18;
+        float length = (float) 78.0332;
+        boolean baby = true;
+        System.out.println(length);
     }
 }
